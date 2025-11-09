c332a0c9105778e7f40236207903f5f09e57cc2d
Neha_singh <nehasinghagra0101@gmail.com>
2025-11-09 16:49:14 +0530

    Fix JSX class attributes in Header component
    Updated class attributes to className for JSX compliance.

--- src/my_components/Header.jsx
+++ src/my_components/Header.jsx
@@ -4,24 +4,24 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <>
-      <nav class="navbar fixed-top">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#slidingNavbar" aria-controls="slidingNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar fixed-top">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#slidingNavbar" aria-controls="slidingNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse custom-sliding-menu " id="slidingNavbar">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 custom-nav-links">
-              <li class="nav-item">
-                <Link class="nav-link active" to="/home">Home</Link>
+          <div className="collapse navbar-collapse custom-sliding-menu " id="slidingNavbar">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 custom-nav-links">
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">Home</Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/about">About Us</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About Us</Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/services">Services</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/services">Services</Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/contact">Contact</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact</Link>
               </li>
             </ul>
           </div>
