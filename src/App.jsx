ec73b9f375ec639b7f5d402ca4e95e15ea10789c
Neha_singh <nehasinghagra0101@gmail.com>
2025-11-09 14:34:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/services" element={<ServicesPage />} />
